926731940287428ea4c672e351b783e47f48577b
Rithik Kapoor <rithikkapoor25@gmail.com>
2021-04-14 00:02:07 +0530

    Encyclopedia Structure Done

---
+++ app/src/main/java/com/example/trawler/Fish.java
@@ -0,0 +1,33 @@
+package com.example.trawler;
+
+public class Fish {
+    String name;
+    double size;
+    double weight;
+    public Fish(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSize(double size) {
+        this.size = size;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
+
